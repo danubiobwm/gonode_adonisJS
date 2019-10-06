4f7dc8a81f0005a3f9da701cc75dfb5a3322efdb
danubiobwm <danubio.bwm@gmail.com>
2019-10-06 11:20:12 -0300

    Internationalization

--- app/Validators/Task.js
+++ app/Validators/Task.js
@@ -1,11 +1,15 @@
 'use strict'
-
+const Antl = use('Antl')
 class Task {
   get rules () {
     return {
       title: 'required',
       due_data: 'date'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation.json')
   }
 }
 
